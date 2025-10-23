04292688f418ac8ffe8f0fe88f8f950a09e68297
TS <t.sappalunn@gmail.com>
2025-10-23 23:34:37 +0700

    fix: update MONGO_URI to remove default value

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
 export const config = {
   PORT: Number(process.env.PORT ?? 3000),
-  MONGO_URI: process.env.MONGO_URI ?? "mongodb://localhost:27017/products",
+  MONGO_URI: process.env.MONGO_URI,
 };
